f19b0e6805abf2e8fc7d6a0ddef83c3dfade2bbd
vishnukkrishna <vishnukkrishna1999@gmail.com>
2023-06-03 10:03:11 +0530

    Second commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+
+    name=models.CharField(max_length=200)
+
+    description=models.TextField(blank=True)
+
+    pages=models.IntegerField()
+
+    price=models.DecimalField( max_digits=10, decimal_places=2)
+
+    auther=models.CharField(max_length=100,null=True)
+
+
+    def __str__(self):
+
+        return self.name
+    
+
+    class Meta:
+
+        verbose_name_plural='books'
+    
+
+
+class Users(models.Model):
+
+    username=models.CharField(max_length=20)
+
+    Book=models.ForeignKey(Book,on_delete=models.CASCADE)
+
+
+
+    def __str__(self):
+
+        return self.username
+    
+
+    class Meta:
+
+        verbose_name_plural='users'
