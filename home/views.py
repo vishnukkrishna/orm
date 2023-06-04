d5fb3c2262861bec077065d4303904ccef53ec5b
vishnukkrishna <vishnukkrishna1999@gmail.com>
2023-06-04 19:19:40 +0530

    Third Commit

--- home/views.py
+++ home/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .models import Book, Users
+from django.db import connection
 # Create your views here.
 
 
 def home(request):
     # post = Book.objects.all()
     
-    # print(connection.quries)
+    
     post = Book.objects.raw("select * from home_Book")
     print(post)
+    cursor = connection.cursor()
+    cursor.execute("select * from home_Book")
+    r = cursor.fetchone()
+    print(r)
+    print(connection.queries)
     return render(request, 'output.html', {'post': post})
