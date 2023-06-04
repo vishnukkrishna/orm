5653d6ca8a69e2f5dd7f3c89e12fd7eb23cb2c92
vishnukkrishna <vishnukkrishna1999@gmail.com>
2023-06-04 10:23:54 +0530

    Small changes

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .models import Book, Users
 # Create your views here.
 
 
 def home(request):
-    pass
+    # post = Book.objects.all()
+    
+    # print(connection.quries)
+    post = Book.objects.raw("select * from home_Book")
+    print(post)
+    return render(request, 'output.html', {'post': post})
--- orm/settings.py
+++ orm/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'orm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
